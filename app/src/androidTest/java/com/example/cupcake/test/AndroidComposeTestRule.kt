fad539c2e322258b0dc1f2e493d11162939f465d
Marcelo Rocha <contato@omarcelo.com.br>
2025-05-09 20:43:42 -0300

    Adding tests

---
+++ app/src/androidTest/java/com/example/cupcake/test/AndroidComposeTestRule.kt
@@ -0,0 +1,41 @@
+package com.example.cupcake.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.example.cupcake.ui.SelectOptionScreen
+import com.example.cupcake.R
+import org.junit.Rule
+import org.junit.Test
+
+@get:Rule
+val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+class AndroidComposeTestRule {
+    @Test
+    fun selectOptionScreen_verifyContent() {
+        // Given list of options
+        val flavors = listOf("Vanilla", "Chocolate", "Hazelnut", "Cookie", "Mango")
+        // And subtotal
+        val subtotal = "$100"
+
+        // When SelectOptionScreen is loaded
+        composeTestRule.setContent {
+            SelectOptionScreen(subtotal = subtotal, options = flavors)
+        }
+
+        // Then all the options are displayed on the screen.
+        flavors.forEach { flavor ->
+            composeTestRule.onNodeWithText(flavor).assertIsDisplayed()
+        }
+
+        // And then the subtotal is displayed correctly.
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.subtotal_price,
+                subtotal
+            )
+        ).assertIsDisplayed()
+
+        // And then the next button is disabled
+        composeTestRule.onNodeWithStringId(R.string.next).assertIsNotEnabled()
+    }
+}
